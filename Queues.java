8e10766beddc39db11a0143844401766ba4092ca
Sarankumar <91582966+SarankumarSelvaraj@users.noreply.github.com>
2023-01-16 22:05:08 +0530

    added the new file

--- Queues.java
+++ Queues.java
@@ -1,17 +1,5 @@
-/*Queue is a linear data structure and that is store the data in queue or line. It is first in first out(fifo). 
-fifo means which data is first stored and that data is removed first. In technical term we called two things one is front and rear. front is first element of the queue.
-rear is the last element of the queue. 
-for example, you have to withdraw the money in ATM so you go to ATM and see there is thirty people is standing in the queue 
-infront of you. front person is leaved form the ATM after withdraw the money who is come first in the atm. and that person is leaved after that second person 
-who is stand next to the front person is going to withdraw the money and leave from the ATM. after the second guy and next one is third guy who is wait next to second 
-person is going to withdraw the money and leave from the ATM,......etc. this process will take until it reaches rear point.
- */
-/*Queue is interface class so we instantied the queue class from the other collection frameworks such as LinkedList, PriorityQueue, etc. but in that case we define our
- own class inside that write the different methods as our own implementation. */
 class QueueOrder {
-  // declare the front, rear, capacity as globally.
     private static int front, rear, capacity;
-  // declare the queue array as globally.
     private static int queue[];
     // create the constructor for queue order class.
     QueueOrder(int size) {
@@ -19,20 +7,15 @@ class QueueOrder {
         capacity = size;
         queue = new int[capacity];
     }
-  // write the enqueue method to add the element to the queue.
     public void enqueue(int item) {
-      // here if the size of the queue is full, it will print the queue is full.
       if(capacity == rear) {
         System.out.println("Queue is full.");
       }
-      // else, add the new element to the queue.
       else {
         queue[rear] = item;
-        // increment the element to the queue.
         rear++;
       }
     }
-  // dequeue method is used to delete the element from the queue.
     public void dequeue() {
         if(front == rear) {
             System.out.println("Queue is empty, so there is no need to delete");
@@ -44,11 +27,9 @@ class QueueOrder {
 
         if(rear < capacity)
             queue[rear] = 0;
-          // decrement the element from the queue.
             rear--;
     }
 }
-  // display the all the elements of the queue.
     public void display() {
         if(front == rear) {
             System.out.println("Queue is empty.");
@@ -59,7 +40,6 @@ class QueueOrder {
             }
         }
     }
-  // frontQueue method is used to print the first element of the queue.
     public void frontQueue() {
         if(front == rear) {
             System.out.println("Queue is empty");
@@ -69,7 +49,6 @@ class QueueOrder {
 }
 public class Queues {
    public static void main(String[] args) {
-     // we instantied the QueueOrder class and create the object for that class and perform the different types of method in the queue main class.
     QueueOrder q1 = new QueueOrder(6);
     // added the elements to the queue.
     q1.enqueue(1);
@@ -85,7 +64,7 @@ public class Queues {
    q1.dequeue();
    q1.display();
    System.out.println();
-   // display the first element of the queue.
+   // display the front element of the queue.
    q1.frontQueue();
 }
 }
